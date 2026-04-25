caf40ca2a0036c4e0a16f4b13c4d34dc4d830907
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-25 10:51:56 +0200

    Fix syntax errors in Dev.sh conditional statements

--- Module/Dev.sh
+++ Module/Dev.sh
@@ -5,8 +5,7 @@ echo "Updating systeme..."
 sudo apt update &> /dev/null
 
 #chromium
-if [[ "$install_chromium" == true]];
-then
+if [[ "$install_chromium" == true ]]; then
     if command -v chromium &> /dev/null;
     then
         echo "Chromium is already install"
@@ -17,8 +16,7 @@ then
 fi
 
 #vscode
-if [[ "$install_code" == true ]];
-then
+if [[ "$install_code" == true ]]; then
     if command -v code &> /dev/null;
     then
         echo "Vscode is already install"
@@ -30,38 +28,31 @@ then
         cd vscode
         mkdir data
 
-        if [[ "$install_python_extension_for_vscode" == true ]];
-        then
+        if [[ "$install_python_extension_for_vscode" == true ]]; then
             code --install-extension ms-python.python
         fi
 
-        if [[ "$install_c_extension_for_vscode" == true ]];
-        then
+        if [[ "$install_c_extension_for_vscode" == true ]]; then
             code --install-extension ms-vscode.cpptools
         fi
 
-        if [[ "$install_run_extension_for_vscode" == true ]];
-        then
+        if [[ "$install_run_extension_for_vscode" == true ]]; then
             code --install-extension formulahendry.code-runner
         fi
     fi
 fi
 
 #python3
-if [[ "$install_python3" == true ]];
-then
-    if command -v python3 &> /dev/null;
-    then 
+if [[ "$install_python3" == true ]]; then
+    if command -v python3 &> /dev/null; then 
         echo "Python is already install"
     else
         echo "Python is missing"
         read -p "Do you want to install it ? [yes or no] : " choice
-        if [[ "$choice" == "yes" ]];
-        then
+        if [[ "$choice" == "yes" ]]; then
         echo "Installing python3..."
             sudo apt install -y python3
-        elif [[ "$choice" == "y" ]];
-        then 
+        elif [[ "$choice" == "y" ]]; then 
             echo "Please type yes"
         else
             echo "Installation cancelled"
@@ -70,21 +61,17 @@ then
 fi
 
 #NodeJS
-if [[ "$install_nodejs" == true ]]:
-then
+if [[ "$install_nodejs" == true ]]; then
 
-    if command -v node &> /dev/null;
-    then
+    if command -v node &> /dev/null; then
         echo "NodeJS is already install"
     else
         echo "NodeJS is missing"
         read -p "Do you want to install it ? [yes or non] :" choice
-        if [[ "$choice" == "yes" ]];
-        then
+        if [[ "$choice" == "yes" ]]; then
         echo "Installing NodeJS..."
             sudo apt install -y node
-        elif [[ "$choice" == "y" ]];
-        then 
+        elif [[ "$choice" == "y" ]]; then 
             echo "Please type yes"
         else
             echo "Installation cancelled"
