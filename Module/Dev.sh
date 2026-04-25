2fc828c4050d7164e5fd757d1a07b36538c36031
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-25 14:22:13 +0200

    Fix syntax and update installation commands in Dev.sh

--- Module/Dev.sh
+++ Module/Dev.sh
@@ -5,7 +5,8 @@ echo "Updating systeme..."
 sudo apt update &> /dev/null
 
 #chromium
-if [[ "$install_chromium" == true ]]; then
+if [[ "$install_chromium" == true]];
+then
     if command -v chromium &> /dev/null;
     then
         echo "Chromium is already install"
@@ -16,43 +17,51 @@ if [[ "$install_chromium" == true ]]; then
 fi
 
 #vscode
-if [[ "$install_code" == true ]]; then
+if [[ "$install_code" == true ]];
+then
     if command -v code &> /dev/null;
     then
         echo "Vscode is already install"
     else
         echo "Installing Vscode..."
-        wget -O vscode.tar.gz https://update.code.visualstudio.com/latest/linux-x64/stable
-        tar -xzf vscode.tar.gz -d vscode
-        rm vscode.tar.gz
+        wget -O vscode.zip https://update.code.visualstudio.com/latest/linux-x64/stable
+        unzip vscode.zip -d vscode
+        rm vscode.zip
         cd vscode
         mkdir data
 
-        if [[ "$install_python_extension_for_vscode" == true ]]; then
+        if [[ "$install_python_extension_for_vscode" == true ]];
+        then
             code --install-extension ms-python.python
         fi
 
-        if [[ "$install_c_extension_for_vscode" == true ]]; then
+        if [[ "$install_c_extension_for_vscode" == true ]];
+        then
             code --install-extension ms-vscode.cpptools
         fi
 
-        if [[ "$install_run_extension_for_vscode" == true ]]; then
+        if [[ "$install_run_extension_for_vscode" == true ]];
+        then
             code --install-extension formulahendry.code-runner
         fi
     fi
 fi
 
 #python3
-if [[ "$install_python3" == true ]]; then
-    if command -v python3 &> /dev/null; then 
+if [[ "$install_python3" == true ]];
+then
+    if command -v python3 &> /dev/null;
+    then 
         echo "Python is already install"
     else
         echo "Python is missing"
         read -p "Do you want to install it ? [yes or no] : " choice
-        if [[ "$choice" == "yes" ]]; then
+        if [[ "$choice" == "yes" ]];
+        then
         echo "Installing python3..."
             sudo apt install -y python3
-        elif [[ "$choice" == "y" ]]; then 
+        elif [[ "$choice" == "y" ]];
+        then 
             echo "Please type yes"
         else
             echo "Installation cancelled"
@@ -61,21 +70,35 @@ if [[ "$install_python3" == true ]]; then
 fi
 
 #NodeJS
-if [[ "$install_nodejs" == true ]]; then
+if [[ "$install_nodejs" == true ]]:
+then
 
-    if command -v node &> /dev/null; then
+    if command -v node &> /dev/null;
+    then
         echo "NodeJS is already install"
     else
         echo "NodeJS is missing"
         read -p "Do you want to install it ? [yes or non] :" choice
-        if [[ "$choice" == "yes" ]]; then
+        if [[ "$choice" == "yes" ]];
+        then
         echo "Installing NodeJS..."
             sudo apt install -y node
-        elif [[ "$choice" == "y" ]]; then 
+        elif [[ "$choice" == "y" ]];
+        then 
             echo "Please type yes"
         else
             echo "Installation cancelled"
         fi
     fi
 
+fi
+
+if [[ "$install_git" == true ]]; then
+    
+    if command -v git &> /dev/null; then
+        echo "Git is already install"
+    else
+        echo "Installing Git..."
+        sudo apt install -y git
+    fi
 fi
