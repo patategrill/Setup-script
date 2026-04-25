94d57ac894eac2b79865bb0fffa1c8425a5bedbc
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-25 20:16:43 +0200

    Add option for Office Software installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source config.conf
 
 echo "choose option :"
 echo "1) Dev"
 echo "2) CTF"
+echo "3) Office Software"
 read choice
 
 if [[ "$choice" == "1" ]];
@@ -14,6 +14,10 @@ elif [[ "$choice" == "2" ]];
 then
     echo "Installing CTF configuration..."
     source Module/CTF.sh
+elif [[ "$choice" == "3" ]];
+then
+    echo "Installing Office Software configuration..."
+    source Module/OfficeSoftware.sh
 else
     echo "Invalid input"
 fi
