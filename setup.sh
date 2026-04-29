37d3c4f09134180e3152b61935535cb2843125fb
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-29 18:14:49 +0200

    Refactor setup script for improved user options

--- setup.sh
+++ setup.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
 
-echo "choose option :"
+echo "Choose option :"
 echo "1) Dev"
 echo "2) CTF"
 echo "3) Office Software"
-read choice
+read choice1
 
-if [[ "$choice" == "1" ]];
+if [[ "$choice1" == "1" ]];
 then 
-    echo "Installing Dev configuration..."
-    source Module/Dev.sh
-elif [[ "$choice" == "2" ]];
+    echo "Choose setup mode :"
+    echo "1) Minimal"
+    echo "2) All"
+    read choice2
+    if [[ "$choice2" == "1" ]];
+    then
+        echo "Installing Minimal Dev configuration..."
+        source Module/DevMinimal.sh
+    elif [[ "$choice2" == "2" ]];
+    then
+        echo "Installing All Dev configuration..."
+        source Module/DevAll.sh
+    else
+        echo "Invalid input"
+    fi
+elif [[ "$choice1" == "2" ]];
 then
-    echo "Installing CTF configuration..."
-    source Module/CTF.sh
-elif [[ "$choice" == "3" ]];
+    echo "Choose setup mode :"
+    echo "1) Minimal"
+    echo "2) All"
+    read choice2  
+    if [[ "$choice2" = "1" ]];
+    then  
+        echo "Installing Minimal CTF configuration..."
+        source Module/CTFMinimal.sh
+    elif [[ "$choice2" == "2" ]];
+    then
+        echo "Installing All CTF configuration..."
+        source Module/CTFAll.sh
+    else
+        echo "Invalid input"
+    fi
+elif [[ "$choice1" == "3" ]];
 then
-    echo "Installing Office Software configuration..."
-    source Module/OfficeSoftware.sh
+    echo "Choose setup mode :"
+    echo "1) Minimal"
+    echo "2) All"
+    read choice2
+    if [[ "$choice2" == "1" ]];
+    then
+        echo "Installing Minimal Office Software configuration..."
+        source Module/OfficeSoftwareMinimal.sh
+    elif [[ "$choice2" == "2" ]];
+    then
+        echo "Installing All Office Software configuration..."
+        source Module/OfficeSoftwareAll.sh
+    else
+        echo "Invalid input"
+    fi
 else
     echo "Invalid input"
 fi
