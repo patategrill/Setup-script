a38787f9111ce1910b65a04d4f6f44d712d0309a
patategrill <194829329+patategrill@users.noreply.github.com>
2026-05-10 21:26:49 +0200

    Update DevMinimal.sh

--- Module/DevMinimal.sh
+++ Module/DevMinimal.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 source ../Config/configOfficeSoftware.conf
 
+#Spotify
+if [[ "$install_spotify" == true ]];
+then
+    if command -v spotify &> /dev/null;
+    then
+        echo "Spotify is already install"
+    else
+        echo "Installing Spotify..."
+        if command -v flatpak &> /dev/null;
+        then
+            flatpak install -y flathub com.spotify.Client &> /dev/null
+        else
+            echo "Flatpak is not installed. Do you want to install it ? (y/n)"
+            read -r answer
+            if [[ "$answer" == "y" ]]; then
+                sudo $distro install -y flatpak &> /dev/null
+                flatpak install -y flathub com.spotify.Client &> /dev/null
+                sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &> /dev/null
+            else
+                echo "Spotify installation skipped."
+            fi
+        fi
+    fi
+    echo "Spotify installation successful"
+fi
+
 #vscode
 if [[ "$install_code" == true ]];
 then
