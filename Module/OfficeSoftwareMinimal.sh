927c1a52dd29cade2549d901394b37140af5c670
patategrill <194829329+patategrill@users.noreply.github.com>
2026-05-10 20:58:16 +0200

    Update OfficeSoftwareMinimal.sh

--- Module/OfficeSoftwareMinimal.sh
+++ Module/OfficeSoftwareMinimal.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 source ../Config/configOfficeSoftware.conf
 
-echo "Updating systeme..."
-sudo apt update &> /dev/null
-
 #libreOffice
 if [[ "$install_LibreOffice" == true ]]; then
     if command -v libreoffice &> /dev/null; then
         echo "libreOffice is already install"
     else
         echo "Installing LibreOffice..."
-        sudo apt install -y libreoffice
+        sudo $distro install -y libreoffice
     fi
     echo "LibreOffice installation successful"
 fi
