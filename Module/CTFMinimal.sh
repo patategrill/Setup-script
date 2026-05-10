57217019c5b118441253b36befbf9960bea43231
patategrill <194829329+patategrill@users.noreply.github.com>
2026-05-10 21:26:19 +0200

    Add Spotify and Wireshark installation scripts

--- Module/CTFMinimal.sh
+++ Module/CTFMinimal.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 source ../Config/configOfficeSoftware.conf
 
+#Spotify
+if [[ "$install_spotify" == true ]];
+then
+    if command -v spotify &> /dev/null;
+    then
+        echo "Spotify is already install"
+    else
+        echo "Installing Spotify..."
+        if command -v flatpak &> /dev/null;
+        then
+            flatpak install -y flathub com.spotify.Client &> /dev/null
+        else
+            echo "Flatpak is not installed. Do you want to install it ? (y/n)"
+            read -r answer
+            if [[ "$answer" == "y" ]]; then
+                sudo $distro install -y flatpak &> /dev/null
+                flatpak install -y flathub com.spotify.Client &> /dev/null
+                sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &> /dev/null
+            else
+                echo "Spotify installation skipped."
+            fi
+        fi
+    fi
+    echo "Spotify installation successful"
+fi
 
 #Ghidra
 if [[ "$install_ghidra" == true ]];
@@ -26,6 +51,7 @@ then
     git clone https://github.com/RykerWilder/rockyou.txt.git &> /dev/null
 fi
 
+#Wireshark
 if [[ "$install_wireshark" == true ]];
 then
     if command -v wireshark &> /dev/null;
@@ -36,30 +62,4 @@ then
         sudo $distro install -y wireshark &> /dev/null
     fi
     echo "Wireshark installation successful"
-fi
-
-
-#SqlMap
-if [[ "$install_sqlmap" == true ]]; then
-    if command -v sqlmap &> /dev/null; then
-        echo "SqlMap is already install"
-    else
-        echo "Installing SqlMap..."
-        sudo git clone https://github.com/sqlmapproject/sqlmap.git /opt/sqlmap &> /dev/null;
-        sudo ln -s /opt/sqlmap/sqlmap.py /usr/local/bin/sqlmap
-        sudo chmod +x /opt/sqlmap/sqlmap.py
-    fi
-    echo "SqlMap installation successful"
-fi
-
-
-#Nmap
-if [[ "$install_nmap" == true ]]; then
-    if command -v nmap &> /dev/null; then
-        echo "Nmap is already install"
-    else
-        echo "Installing Nmap..."
-        sudo $distro install -y nmap &> /dev/null
-    fi
-    echo "Nmap installation successful"
 fi
